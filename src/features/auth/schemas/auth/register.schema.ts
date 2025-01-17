21436e37cb30359fb9207bc8837bd29ceaa52908
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-01-17 21:40:45 +0100

    s

--- src/features/auth/schemas/auth/register.schema.ts
+++ src/features/auth/schemas/auth/register.schema.ts
@@ -1,6 +1,8 @@
 import S from "fluent-json-schema";
 
 export const CreateUserInputType = S.object()
+  .id("#CreateUserInputType")
+  .title("CreateUserInputType")
   .prop("email", S.string().format("email").required())
   .prop("password", S.string().minLength(5).required())
   .prop("username", S.string().minLength(5).required());
