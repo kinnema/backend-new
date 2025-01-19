7a893306fce75ee6023f0c4d11b348ce5304a359
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-01-19 16:16:18 +0100

    feat: update CORS configuration to use environment variable for frontend URL
    - Changed CORS origin setting in cors.plugin.ts to utilize FRONTEND_URL from environment variables, enhancing flexibility for different deployment environments.

--- src/plugins/cors.plugin.ts
+++ src/plugins/cors.plugin.ts
@@ -3,7 +3,7 @@ import fp from "fastify-plugin";
 
 export default fp(async function (fastify) {
   await fastify.register(cors, {
-    origin: true,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
 });
