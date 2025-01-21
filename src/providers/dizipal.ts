272c3ffb32e70a1599edcd3f2574c7c9ee3636de
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-01-21 22:42:34 +0100

    fix: update DizipalProvider URLs to new domain

--- src/providers/dizipal.ts
+++ src/providers/dizipal.ts
@@ -9,7 +9,7 @@ export default class DizipalProvider extends BaseProvider {
       "dizipal",
       ProviderPriority.HIGH,
       true,
-      "https://dizipal870-dotcom.gateway.web.tr"
+      "https://dizipal871-dotcom.gateway.web.tr"
     );
   }
 
@@ -21,9 +21,9 @@ export default class DizipalProvider extends BaseProvider {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
         Cookie:
-          "dizipal870_com=%7B%22HttpHost%22%3A%22dizipal870.com%22%2C%22Protokol%22%3A%22https%22%2C%22Port%22%3A443%2C%22KulAdSifre%22%3Anull%2C%22UrlAdresi%22%3A%22%5C%2Fdizi%5C%2Fgassal%5C%2Fsezon-1%5C%2Fbolum-1%22%2C%22GetVeri%22%3Anull%2C%22GitOpjeId%22%3Anull%2C%22DnsAdresi%22%3A%2213%22%2C%22URL_Adresi%22%3A%22https%3A%5C%2F%5C%2Fdizipal870.com%5C%2Fdizi%5C%2Fgassal%5C%2Fsezon-1%5C%2Fbolum-1%22%2C%22GirisIP%22%3A%22104.21.112.1%22%7D",
-        Origin: "https://dizipal870.com",
-        Referer: "https://dizipal870.com",
+          "dizipal871_com=%7B%22HttpHost%22%3A%22dizipal871.com%22%2C%22Protokol%22%3A%22https%22%2C%22Port%22%3A443%2C%22KulAdSifre%22%3Anull%2C%22UrlAdresi%22%3A%22%5C%2Fdizi%5C%2Fgassal%5C%2Fsezon-1%5C%2Fbolum-1%22%2C%22GetVeri%22%3Anull%2C%22GitOpjeId%22%3Anull%2C%22DnsAdresi%22%3A%2213%22%2C%22URL_Adresi%22%3A%22https%3A%5C%2F%5C%2Fdizipal871.com%5C%2Fdizi%5C%2Fgassal%5C%2Fsezon-1%5C%2Fbolum-1%22%2C%22GirisIP%22%3A%22104.21.112.1%22%7D",
+        Origin: "https://dizipal871.com",
+        Referer: "https://dizipal871.com",
       };
 
       const url = `${this.providerUrl}/dizi/${params.serie_name}/sezon-${params.season_number}/bolum-${params.episode_number}`;
