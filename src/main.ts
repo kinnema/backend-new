0f34690462e2dba818ca29855ff07bf29a7ca485
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2025-06-20 22:40:52 +0200

    chore: remove deprecated watch feature and associated providers

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import "dotenv/config";
 import fastify from "fastify";
 import path from "path";
 import { initModules } from "./core/main/init_modules";
-import DizipalProvider from "./providers/dizipal";
-import DizipalBrowserProvider from "./providers/dizipal_browser";
-import { providerRegistry } from "./providers/provider.registry";
-import { VidsrcProvider } from "./providers/vidsrc.provider";
 
 export const BASE_PATH = path.join(__dirname);
 
@@ -14,10 +10,6 @@ const app = fastify({
 });
 
 initModules(app);
-
-providerRegistry.registerProvider(new DizipalProvider());
-providerRegistry.registerProvider(new DizipalBrowserProvider());
-providerRegistry.registerProvider(new VidsrcProvider());
 
 const start = async () => {
   try {
